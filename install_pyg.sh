da592953faecc3f0fcb6ad5fac6b2564ea3165fd
Michelle M. Li <michelle@MBI-ZITN-L00898.local>
2022-11-07 18:17:15 -0500

    Add files required to set up environment

---
+++ install_pyg.sh
@@ -0,0 +1,5 @@
+pip install torch-scatter==2.0.8 -f https://pytorch-geometric.com/whl/torch-1.8.0+cu102.html
+pip install torch-sparse==0.6.11 -f https://pytorch-geometric.com/whl/torch-1.8.0+cu102.html
+pip install torch-cluster==1.5.9 -f https://pytorch-geometric.com/whl/torch-1.8.0+cu102.html
+pip install torch-spline-conv==1.2.1 -f https://pytorch-geometric.com/whl/torch-1.8.0+cu102.html
+pip install torch-geometric==1.7.2
